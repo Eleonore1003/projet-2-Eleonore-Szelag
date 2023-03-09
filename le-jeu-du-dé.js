3ab6f6104e8d6f4043b7595cb101b7851037810a
Eleonore Szelag <eleonoreslg02@gmail.com>
2023-03-09 17:09:09 +0100

    modif JS

---
+++ le-jeu-du-dé.js
@@ -0,0 +1,16 @@
+/*
+V1 caractéristiques du jeu
+1 seul écran
+Score ROUND et GLOBAL
+Chaque tour ROUND initialisé à 0
+Lancé de dé autant de fois
+Résultat affiché au ROUND
+
+V2 : option du joueur
+Cliquer sur "Hold" pour envoyer les points du ROUND vers GLOBAL -> Tour de l'autre jouer
+Lancé du dé : 1= scrore ROUND perdu donc fin de son tour
+Message "Vous avez gagné" ou "Vous avez perdu"
+
+V3 : gagnant
+Le premier joueur à 100 points sur GLOBAL
+*/
